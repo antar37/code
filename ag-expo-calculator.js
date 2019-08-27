fc541695ae0eb7803543fa7bf2f5124c3e0e6360
Alex Tarasiuk <antar37@gmail.com>
2019-08-27 18:55:12 -0500

    added Event Listener

--- ag-expo-calculator.js
+++ ag-expo-calculator.js
@@ -1,6 +1,12 @@
+let form = document.querySelector("#ag-expo-form");
 let furnitureTotal = document.querySelector("#furniture-total");
 let quantityList = document.querySelectorAll("#furnitureList .product-block .quantity");
 
-quantityList.forEach(function(qty){
-   furnitureTotal.value = Number(furnitureTotal.value) + (Number(qty.parentElement.parentElement.querySelector(".price").textContent) * Number(qty.value));
-});
+// Event Listener
+form.addEventListener("input", updateForm);
+
+function updateForm(){
+   quantityList.forEach(function(qty){
+      furnitureTotal.value = Number(furnitureTotal.value) + (Number(qty.parentElement.parentElement.querySelector(".price").textContent) * Number(qty.value));
+   });
+}
